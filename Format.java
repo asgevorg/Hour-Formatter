1a6d9123f96a57dbcdeffa719255445c27162b05
gevorg <gevorg.asatryan2.y@tumo.org>
2022-04-27 13:48:16 +0400

    no duplicate

--- Format.java
+++ Format.java
@@ -5,14 +5,5 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Format extends Time{
-    public void print(){
-        Date date = new Date();
-        Calendar calendar = GregorianCalendar.getInstance();
-        calendar.setTime(date);
-        int hours = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-        System.out.printf("%02d", hours);
-        System.out.print(":");
-        System.out.printf("%02d", minute);
-    }
+
 }
